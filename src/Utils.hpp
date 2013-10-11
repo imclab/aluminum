1ca73eebe7fffa34a1cf0bfd1eec6968d40803e7
Angus Forbes <angus.forbes@mat.ucsb.edu>
2013-10-11 09:13:54 -0700

    Removed Utils.cpp (merged into hpp file)

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -3,47 +3,101 @@
 
 #include "Includes.hpp"
 
-/*
-#include <string>
-#include <vector>
-
-#include "Texture.hpp"
-*/  
-//#include <glm/glm.hpp>
-
 namespace aluminum {
-
-  using std::vector;
-  using glm::vec3;
-  using glm::vec4;
-
+  
   class Utils {
-    public:
-      Utils();
+  public:
     
-      static long randomLong(long min, long max);
-      static int randomInt(int min, int max);
-      static vector<int> randomInts(int howMany, int min, int max);
-      static float randomFloat(float min, float max);
-      static vector<float> randomFloats(int howMany, float min, float max);
-
+    static void randomSeed() {
+      srand((unsigned)time(0));
+    }
+    
+    static float random() {
+      return glm::linearRand(0.0,1.0);
+    }
+    
+    static long randomLong(long min, long max) {
+      //seems like a bit of a bug for negative ints... this fixes it though
+      if (min < 0) {
+        min = min-1;
+      }
       
-      static vec3 randomVec3(float min, float max);
-
-      static vec4 randomColor();
-      static vec4 randomColor(float alpha);
-      static vec4 randomColor(float min, float max);
-      static vec4 randomColor(float min, float max, float alpha);
-
-      static void randomSeed();
-
-      static float random();
-
-    private:
-
+      if (max > 0) {
+        max = max+1;
+      }
+      
+      return (long) glm::linearRand((double)min, (double)max);
+      // return min + int( ((max-min)+1) * random() );
+    }
+    
+    static int randomInt(int min, int max) {
+      //seems like a bit of a bug for negative ints... this fixes it though
+      if (min < 0) {
+        min = min-1;
+      }
+      
+      if (max > 0) {
+        max = max+1;
+      }
+      
+      return (int) glm::linearRand((double)min, (double)max);
+      // return min + int( ((max-min)+1) * random() );
+    }
+    
+    static std::vector<int> randomInts(int howMany, int min, int max) {
+      std::vector<int> ints;
+      
+      for(int index = 0; index < howMany; index++){
+        ints.push_back( randomInt(min,max) );
+      }
+      
+      return ints;
+    }
+    
+    static float randomFloat(float min, float max) {
+      return (float) glm::linearRand(min, max);
+    }
+    
+    static std::vector<float> randomFloats(int howMany, float min, float max) {
+      std::vector<float> floats;
+      
+      for(int index = 0; index < howMany; index++){
+        floats.push_back(randomFloat(min, max));
+      }
+      
+      return floats;
+    }
+    
+    
+    
+    static glm::vec3 randomVec3(float min, float max) {
+      return glm::linearRand(glm::vec3(min), glm::vec3(max));
+    }
+    
+    static glm::vec4 randomColor() {
+      glm::vec3 c = glm::linearRand(glm::vec3(0.0f), glm::vec3(1.0f));
+      return vec4(c.x, c.y, c.z, 1.0);
+    }
+    
+    static glm::vec4 randomColor(float alpha) {
+      glm::vec3 c = glm::linearRand(glm::vec3(0.0f), glm::vec3(1.0f));
+      return glm::vec4(c.x, c.y, c.z, alpha);
+    }
+    
+    static glm::vec4 randomColor(float min, float max) {
+      glm::vec3 c = glm::linearRand(glm::vec3(min), glm::vec3(max));
+      return glm::vec4(c.x, c.y, c.z, 1.0);
+    }
+    
+    static glm::vec4 randomColor(float min, float max, float alpha) {
+      glm::vec3 c = glm::linearRand(glm::vec3(min), glm::vec3(max));
+      return glm::vec4(c.x, c.y, c.z, alpha);
+    }
+    
+    
   };
-
-
+  
+  
 }
 
 #endif
